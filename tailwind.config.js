c78e37306eed1912b08ee343781b40eab122e857
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-14 00:58:32 +0300

    chore: installed enviroment

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./build/**/*.html'],
+  darkMode: 'media',
+  variants: {
+    extend: {},
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Roboto']
+      }
+    },
+  },
+  plugins: [],
+}
