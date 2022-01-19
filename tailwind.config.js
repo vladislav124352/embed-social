47dd2129258c48ca4c75e140cde6020d859e4838
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-19 14:45:16 +0300

    feature: added responsive styles for the "hero" section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
       }
     },
     screens: {
-      xl: '1900px'
+      '2xs': '500px',
+      md: '700px',
+      '2md': '900px',
+      xmd: '1100px',
+      lg: '1200px',
+      xl: '1400px',
+      '2xl': '1900px'
     }
   },
   plugins: [],
