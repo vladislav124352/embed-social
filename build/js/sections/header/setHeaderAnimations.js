941d07ad6be5e3960b3c0aeda74ce50a91c83c73
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-26 23:15:38 +0300

    fix: fixed problem with header animations

--- build/js/sections/header/setHeaderAnimations.js
+++ build/js/sections/header/setHeaderAnimations.js
@@ -2,7 +2,7 @@ export const setHeaderAnimations = () => {
     const header = document.getElementById('header');
 
     window.addEventListener('scroll', (event) => {
-        if (window.scrollY >= 55) {
+        if (window.scrollY >= header.clientHeight) {
             header.classList.remove('header-scroll-relative')
             header.classList.add('header-scroll-sticky')
         } else if (window.scrollY === 0) {
